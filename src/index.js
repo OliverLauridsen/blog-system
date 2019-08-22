d2a47b8860f7f2e8ceda9d9c7f8cf75717237df3
Oliver <olla@html24.net>
2019-08-22 09:04:44 +0200

    Adds project structure and component separation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CommentDetail from './components/CommentDetail';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
