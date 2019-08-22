56d079b33f652da90938b3530d73f84b164ff513
Oliver <olla@html24.net>
2019-08-22 15:38:41 +0200

    Code organization and component nesting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
+import React, {  } from 'react'
 import ReactDOM from 'react-dom';
 import CommentDetail from './components/CommentDetail';
-
+import faker from 'faker';
+import ApprovalCard from './components/ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                <div>
+                    <h4> Warning! </h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45" 
+                    commentText="Lorem" 
+                    imageURL={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 3:45" 
+                    commentText="Ipsum" 
+                    imageURL={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 2:45" 
+                    commentText="Dolores" 
+                    imageURL={faker.image.avatar()}/>
+            </ApprovalCard>
 
         </div>
     );
